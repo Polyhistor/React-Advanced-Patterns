399ec0b6e3fcfe7bc99ede56ec65b38deae2ec08
Polyhistor <pouya.ataei.7@gmail.com>
2020-06-28 14:41:22 +1200

    shallow merge and react clone

--- advanced-react/src/Toggle.tsx
+++ advanced-react/src/Toggle.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 
 import { Switch } from './Switch';
 
-const Toggle = ({ onToggle }: any) => {
+const Toggle = ({ onToggle, children }: any) => {
+  console.log(children);
   const [on, setOn] = useState(false);
 
   const toggle = () => (
@@ -12,17 +13,31 @@ const Toggle = ({ onToggle }: any) => {
     })()
   );
 
-  return (
-    <div>
-      <Switch on={on} onClick={() => toggle()}></Switch>
-    </div>
+  return React.Children.map(children, (childElement) =>
+    React.cloneElement(childElement, {
+      on,
+      toggle,
+    })
   );
 };
+
+// extending our functional component object
+Toggle.On = ({ on, children }: any) => (on ? children : null);
+Toggle.Off = ({ on, children }: any) => (on ? null : children);
+Toggle.Button = ({ on, toggle, ...props }: any) => (
+  <Switch on={on} onClick={toggle} {...props}></Switch>
+);
 
 const Usage = ({
   onToggle = (...args: any[]): void => console.log('onToggle', ...args),
 }): any => {
-  return <Toggle onToggle={onToggle} />;
+  return (
+    <Toggle onToggle={onToggle}>
+      <Toggle.On>The button is on</Toggle.On>
+      <Toggle.Off>The button is off</Toggle.Off>
+      <Toggle.Button></Toggle.Button>
+    </Toggle>
+  );
 };
 
 Usage.title = 'Build Toggle';
