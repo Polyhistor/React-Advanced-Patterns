f52e371bfcf24111008065d62af875003705ca53
Polyhistor <pouya.ataei.7@gmail.com>
2020-11-09 21:53:22 +1300

    logged the fiber

--- advanced-react/src/HOC/index.jsx
+++ advanced-react/src/HOC/index.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { BlogPost } from './BlogPost';
 import { CommentList } from './CommentList';
-import useSWR, { SWRConfig } from 'swr';
+import useSWR from 'swr';
 import { withSubscription } from './withSubscription';
 
 export const HOCExample = () => {
   const { data } = useSWR('https://fakerapi.it/api/v1/images?_width=380');
   const [hocData, setHocData] = useState(data);
+
+  console.log(<CommentList></CommentList>);
 
   useEffect(() => {
     setHocData(data);
